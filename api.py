bb64b1000be73e60f4e39bc9856869e85d2c9375
Khagendra Kumar <itskhagendra@hotmail.com>
2019-05-16 09:07:47 +0530

    Initial commit

---
+++ api.py
@@ -0,0 +1,11 @@
+
+#importing Modules
+from keras.applications import ResNet50
+from keras.preprocessing.image import img_to_array
+from keras.applications import imagenet_utils
+from PIL import Image
+import numpy as np
+import flask
+import io   
+
+
