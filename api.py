321e6f7cd63bcd2ba7da74a875dd47729e6f6b1c
Khagendra Kumar <itskhagendra@hotmail.com>
2019-05-16 13:00:28 +0530

    All Bugs fixed
    -API is working

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 import flask
+import base64
 import io   
 import sys
 
@@ -15,17 +16,19 @@ model = None
 
 #Load Model
 def load_model():
+    #loading model
     global model
     model = ResNet50(weights="imagenet")
+
+    #loading Model Graph
     global graph
     graph = tf.get_default_graph()
 
 def prepare_image(image,target):
     #image Preprocessing Module
 
-
     #Check if Image is in RGB or not if not then convert it using PIL
-    if(image.mode!="RGB"):
+    if(image.mode != "RGB"):
         image=image.convert("RGB")
 
     #image preprocessing to make sure that the imput image size matches the input tensor size
@@ -41,34 +44,42 @@ def prepare_image(image,target):
 
 @app.route("/api",methods=["POST"])
 def predict():
+
     data = {"success":False}
+    #Check that image is uploaded to the server
     if(flask.request.method == "POST"):
         if(flask.request.files.get("image")):
+            #debug Flag
             print("Entered into Core ML Engine",file=sys.stdout)
-
-            #image = flask.request.files["image"].read()
+            #Read the image using PIL 
             image = flask.request.files["image"].read()
-
-            #image = Image.open(io.BytesIO(image))
             image = Image.open(io.BytesIO(image))
 
+            #Process image
             image = prepare_image(image, target=(224, 224))
-            #image = image_preprocess(image,target=(224,224))
-
-            #preds = model.predict(image)
+            #use tf graph to predict the output
             with graph.as_default():
                 preds = model.predict(image)
+
+            #Storing the predicted classes into list
             results = imagenet_utils.decode_predictions(preds)
 
             data["predictions"] = []
-
+            #iterate over the result to find predicted output 
             for(imagenetID,label,prob) in results[0]:
                 r = {"label":label,"probability":float(prob)}
                 data["predictions"].append(r)
 
+            #This flag indicates that process was successful 
             data["success"]=True
 
+    #return Json Data to the users
     return flask.jsonify(data)
+
+
+@app.route("/")
+def home():
+    return flask.render_template("index.html")
 
 
 if __name__ =="__main__":
